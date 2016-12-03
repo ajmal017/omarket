ba71eaaa14197cd0856d9262a20c1cb2e2d1d940
Christophe Alexandre <chris.perso@gmail.com>
2016-12-03 15:10:19 +0100

    experimenting with RxJava

--- src/main/java/Scratchpad.java
+++ src/main/java/Scratchpad.java
@@ -1,152 +1,94 @@
-import com.jimmoores.quandl.DataSetRequest;
-import com.jimmoores.quandl.QuandlSession;
-import com.jimmoores.quandl.Row;
-import com.jimmoores.quandl.TabularResult;
+import io.reactivex.Observable;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.math.BigDecimal;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
-import joinery.DataFrame;
-import org.threeten.bp.LocalDate;
+import static io.reactivex.Observable.combineLatest;
+import static io.reactivex.Observable.merge;
+import static java.lang.Thread.sleep;
 
-import static java.lang.Math.sqrt;
-import static org.omarket.trading.MarketData.createChannelOrderBookLevelOne;
+class RandomWalk implements Function<Boolean, Double> {
+    private final static Logger logger = LoggerFactory.getLogger(RandomWalk.class);
+    private final Double step;
+    private Double state = 0.;
+
+    public RandomWalk(Double initial, Double step) {
+        this.step = step;
+        this.state = initial;
+    }
+
+    @Override
+    public Double apply(Boolean upOrDown) throws Exception {
+        if(upOrDown){
+            logger.info("up one tick");
+        } else {
+            logger.info("down one tick");
+        }
+        state = upOrDown? state + step: state - step;
+        return state;
+    }
+}
+
+class Hysteresis implements Function<Double, Double> {
+    private final Double outputLow;
+    private final Double outputHigh;
+    private final Double thresholdLow;
+    private final Double thresholdHigh;
+    private Double state;
+
+    public Hysteresis(Double outputLow, Double outputHigh, Double thresholdLow, Double thresholdHigh) {
+        this.outputLow = outputLow;
+        this.outputHigh = outputHigh;
+        this.thresholdLow = thresholdLow;
+        this.thresholdHigh = thresholdHigh;
+        state = outputLow;
+    }
+
+    @Override
+    public Double apply(Double value) throws Exception {
+        if(value >= thresholdHigh){
+            state = outputHigh;
+        } else if (value <= thresholdLow){
+            state = outputLow;
+        }
+        return state;
+    }
+}
 
 public class Scratchpad {
     private final static Logger logger = LoggerFactory.getLogger(Scratchpad.class);
-    public static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd hh:mm:ss.SSS");
-    static {
-        DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+    public static void main(String[] args) throws InterruptedException {
+        Observable<Long> clock = Observable.interval(100, TimeUnit.MILLISECONDS, Schedulers.computation());
+
+        final Random random = new Random();
+
+        Double mean = 100.;
+        Double step = 0.1;
+        Observable<Double> randomWalk1 = clock
+                .map(tick -> random.nextBoolean())
+                .map(new RandomWalk(mean, step));
+
+        Observable<Double> randomWalk2 = clock
+                .map(tick -> random.nextBoolean())
+                .map(new RandomWalk(mean, step));
+
+        Observable<Double> signal = Observable.combineLatest(randomWalk1, randomWalk2, (value1, value2) -> value2 - value1);
+        Observable<Double> filtered = signal.map(new Hysteresis(-1., 1., -1., 1.));
+
+        signal.subscribe(value -> {
+            logger.info("raw value = " + value);
+        });
+
+        filtered.subscribe(value -> {
+            logger.info("filtered value = " + value);
+        });
+        sleep(10000);
     }
 
-    public static void main2(String[] args) throws InterruptedException, ParseException, IOException {
-        DateFormat format = new SimpleDateFormat("yyyyMMdd HH:mm:ss:SSS");
-        List rows = new LinkedList();
-        List columns = Arrays.asList("timestamp", "category", "name", "value");
-        DataFrame df = new DataFrame(
-                rows,
-                columns,
-                Arrays.asList(
-                        Arrays.asList(
-                                format.parse("20000101 00:00:04:010"),
-                                format.parse("20000101 00:00:00:061"),
-                                format.parse("20000101 00:00:02:000"),
-                                format.parse("20000101 00:00:00:051"),
-                                format.parse("20000101 00:00:00:121"),
-                                format.parse("20000101 00:00:04:200"),
-                                format.parse("20000101 00:00:01:121"),
-                                format.parse("20000101 00:00:01:200")
-                        ),
-                        Arrays.asList("test", "test", "test", "beta", "beta", "beta", "beta", "beta"),
-                        Arrays.asList("one", "two", "three", "one", "two", "three", "one", "two"),
-                        Arrays.asList(70, 20, 60, 10, 30, 80, 40, 50)
-                )
-        );
-
-        DataFrame indexed_df = df.reindex("timestamp");
-        logger.info("result indexed:\n" + asString(indexed_df));
-
-        String samplingColumnName = "timestamp";
-        df = df.sortBy(samplingColumnName);
-        DataFrame firstRow = df.head(0);
-        Date firstDate = (Date)firstRow.get(0, "timestamp");
-        logger.info("first date" +  firstDate);
-
-        logger.info("result:\n" + asString(df));
-        for(Object fields: df){
-            Object field = ((List)fields).get(0);
-            Date ts = (Date)field;
-            logger.info(format.format(ts));
-        }
-    }
-
-    public static void main(String[] args){
-        DataFrame<Double> df = loadQuandlInstrument("ECB/EURCHF", 200);
-        logger.info("loaded: " + df.percentChange().stddev().get(0, 1)/ sqrt(24*60*60));
-    }
-
-    private static DataFrame<Double> loadQuandlInstrument(String quandlCode, int samples) {
-        QuandlSession session = QuandlSession.create();
-        DataSetRequest.Builder requestBuilder = DataSetRequest.Builder.of(quandlCode).withMaxRows(samples);
-        TabularResult tabularResult = session.getDataSet(requestBuilder.build());
-        Collection<String> columnNames = tabularResult.getHeaderDefinition().getColumnNames();
-        DataFrame<Double> dataFrame = new DataFrame<>(columnNames);
-        for(Row row: tabularResult){
-            LocalDate date = row.getLocalDate("Date");
-            Double value = row.getDouble("Value");
-            Calendar calendar = new GregorianCalendar(date.getYear(), date.getMonthValue() + 1, date.getDayOfMonth());
-            dataFrame.append(calendar.getTime(), Arrays.asList(new Double[]{value}));
-        }
-        return dataFrame;
-    }
-
-    private static void processRecordedTicks(List<String> dirs, Integer ibCode) {
-        String storageDirPathName = String.join(File.separator, dirs);
-        Path storageDirPath = FileSystems.getDefault().getPath(storageDirPathName);
-        Path productStorage = storageDirPath.resolve(createChannelOrderBookLevelOne(ibCode));
-        logger.info("accessing storage: " + productStorage);
-        if(Files.exists(productStorage)) {
-            Map<String, Path> tickFiles = new TreeMap<>();
-            try (Stream<Path> paths = Files.walk(productStorage)) {
-                paths.forEach(filePath -> {
-                    if (Files.isRegularFile(filePath)) {
-                        Pattern yyyymmddhhEnding = Pattern.compile(".*([0-9]{8})\\/([0-9]{2})$");
-                        Matcher matcher = yyyymmddhhEnding.matcher(filePath.toString());
-                        if(matcher.matches()) {
-                            String yyyymmddhh = matcher.group(1) + " " + matcher.group(2);
-                            logger.info("will be processing recorded ticks: " + yyyymmddhh);
-                            tickFiles.put(yyyymmddhh, filePath);
-                        }
-                    }
-                });
-            } catch (IOException e) {
-                logger.error("failed to access recorded ticks for product " + ibCode, e);
-            }
-            for (Map.Entry<String, Path> entry : tickFiles.entrySet()) {
-                String yyyymmddhh = entry.getKey();
-                Path filePath = entry.getValue();
-                String fullLine = null;
-                try (Scanner scanner = new Scanner(filePath, "utf-8")) {
-                    scanner.useDelimiter("\n");
-                    while (scanner.hasNext()) {
-                        String rawLine = scanner.next().trim();
-                        if (!rawLine.equals("")){
-                            fullLine = yyyymmddhh + ":" + rawLine;
-                            String[] fields = fullLine.split(",");
-                            Date timestamp = DATE_FORMAT.parse(fields[0] + "");
-                            Integer volumeBid = Integer.valueOf(fields[1]);
-                            BigDecimal priceBid = new BigDecimal(fields[2]);
-                            BigDecimal priceAsk = new BigDecimal(fields[3]);
-                            Integer volumeAsk = Integer.valueOf(fields[4]);
-                            logger.info("line: " + timestamp + "," + volumeBid + "," + priceBid);
-                        }
-                    }
-                    scanner.close();
-                } catch (IOException e) {
-                    logger.error("unable to access tick file: " + filePath, e);
-                } catch (ParseException e) {
-                    logger.error("unable to parse line: " + fullLine, e);
-                }
-            }
-        }
-    }
-
-    private static String asString(DataFrame df) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        df.writeCsv(baos);
-        baos.close();
-        return baos.toString();
-    }
 }
