5c8fc5c4bd70f65093e3cd5e726aff4d8b9b23da
Christophe Alexandre <chris.perso@gmail.com>
2016-12-23 11:13:16 +0100

    dev

--- src/main/java/org/omarket/trading/verticles/HistoricalDataVerticle.java
+++ src/main/java/org/omarket/trading/verticles/HistoricalDataVerticle.java
@@ -25,7 +25,6 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.*;
 
-import static java.lang.Thread.sleep;
 import static org.omarket.trading.MarketData.*;
 import static rx.Observable.*;
 
