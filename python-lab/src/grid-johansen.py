b966d2953b8982a5c5e69715b3c67cbab83c4fbb
Christophe Alexandre <chris.perso@gmail.com>
2017-01-10 13:55:08 +0100

    dev

--- python-lab/src/grid-johansen.py
+++ python-lab/src/grid-johansen.py
@@ -6,7 +6,6 @@ import pandas
 
 def load_security(path_db, exchange, symbol):
     data_path = os.sep.join([path_db, exchange, symbol[0], symbol])
-    logging.info('path=' + os.path.abspath(data_path))
     dataframes = list()
     header = ('date', 'open', 'high', 'low', 'close', 'close_adj', 'volume')
     date_parser = lambda x: pandas.datetime.strptime(x, '%Y%m%d')
@@ -24,12 +23,11 @@ def load_security(path_db, exchange, symbol):
 
 def list_securities(path):
     for root, dirs, files in os.walk(path):
-        path = root.split(os.sep)
-        for file in files:
-            if file.endswith('.csv'):
-                exchange = path[-3]
-                stock = path[-1]
-                yield exchange, stock
+        if len(path) >= 3 and len([f for f in files if f.endswith('.csv')]) > 0:
+            path = root.split(os.sep)
+            exchange = path[-3]
+            stock = path[-1]
+            yield exchange, stock
 
 
 def main(args):
@@ -38,14 +36,19 @@ def main(args):
         eod_path = '.'
 
     logging.info('using eod path: %s' % os.path.abspath(eod_path))
+    securities = list()
     for count, data in enumerate(list_securities(eod_path)):
         exchange, stock = data
-        logging.info('%s/%s' % (exchange, stock))
-        if count == 10:
-            break
+        securities.append((exchange, stock))
 
-    eod_data = load_security(eod_path, 'PCX', 'HYG')
-    logging.info("result:\n%s" % eod_data)
+    logging.info('%s' % str(securities))
+    df = pandas.DataFrame()
+    for exchange, stock in securities:
+        eod_data = load_security(eod_path, exchange, stock)
+        df['%s/%s' % (exchange, stock)] = eod_data['close_adj']
+
+    logging.info("result:\n%s" % df)
+    df.to_pickle('eod.pkl')
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
