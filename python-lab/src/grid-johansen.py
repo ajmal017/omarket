b5cb27e931b2e5371fc984df7e9ee84baf9b099e
Christophe <ch.alexandre@bluewin.ch>
2017-01-12 10:47:45 +0100

    dev

--- python-lab/src/grid-johansen.py
+++ python-lab/src/grid-johansen.py
@@ -1,13 +1,13 @@
 import logging
 import argparse
 import os
+from datetime import timedelta
+from matplotlib import pyplot
 
 import itertools
-import pickle
 import shelve
 
 import pandas
-from statsmodels.compat import numpy
 
 import cointeg
 
@@ -38,35 +38,34 @@ def list_securities(path):
             yield exchange, stock
 
 
-def prepare_data(args):
-    eod_path = args.eod_path
-    if eod_path is None:
-        eod_path = '.'
-
-    logging.info('using eod path: %s' % os.path.abspath(eod_path))
+def prepare_data(eod_path):
     securities = list()
     for count, data in enumerate(list_securities(eod_path)):
         exchange, stock = data
         securities.append((exchange, stock))
 
-    df = pandas.DataFrame()
+    close_df = pandas.DataFrame()
+    volume_df = pandas.DataFrame()
     for count, data in enumerate(securities):
         exchange, stock = data
         eod_data = load_security(eod_path, exchange, stock)
-        df['%s/%s' % (exchange, stock)] = eod_data['close_adj']
+        close_df['%s/%s' % (exchange, stock)] = eod_data['close_adj']
+        volume_df['%s/%s' % (exchange, stock)] = eod_data['volume']
         logging.info('processed: %s/%s %d/%d' % (exchange, stock, count + 1, len(securities)))
 
-    logging.info("result:\n%s" % df)
-    df.to_pickle(os.sep.join([eod_path, 'eod.pkl']))
+    logging.info("result:\n%s" % close_df)
+    close_df.to_pickle(os.sep.join([eod_path, 'eod.pkl']))
+    volume_df.to_pickle(os.sep.join([eod_path, 'volume.pkl']))
 
 
-def main(args):
-    eod_path = args.eod_path
-    if eod_path is None:
-        eod_path = '.'
-
-    df = pandas.read_pickle(os.sep.join([eod_path, 'eod.pkl']))
-    columns = [column for column in df.columns if column.startswith('PCX/')]
+def generate_data(eod_path):
+    pyplot.style.use('ggplot')
+    close_df = pandas.read_pickle(os.sep.join([eod_path, 'eod.pkl']))
+    volume_df = pandas.read_pickle(os.sep.join([eod_path, 'volume.pkl']))
+    recent_history = volume_df.index.max() - timedelta(days=60)
+    median_volumes = volume_df[volume_df.index > recent_history].fillna(0).median()
+    most_traded = median_volumes[median_volumes > 1E6].keys().values.tolist()
+    columns = [column for column in most_traded if column.startswith('PCX/')]
     logging.info('combining %d series' % len(columns))
 
     def to_key(symbols):
@@ -76,7 +75,7 @@ def main(args):
         with shelve.open(os.sep.join([eod_path, 'results'])) as results:
             if to_key(combination) not in results:
                 logging.info('processing: %s' % str(combination))
-                current_df = df[list(combination)].dropna()
+                current_df = close_df[list(combination)].dropna()
                 try:
                     result = cointeg.cointegration_johansen(current_df)
                     keepers = ('eigenvectors', 'trace_statistic', 'eigenvalue_statistics', 'critical_values_trace', 'critical_values_max_eigenvalue')
@@ -85,8 +84,21 @@ def main(args):
                 except Exception as err:
                     logging.error('failed for combination: %s' % str(combination))
 
-        if count == 100000:
+        if count == 100:
             break
+
+
+def main(args):
+    eod_path = args.eod_path
+    logging.info('using eod path: %s' % os.path.abspath(eod_path))
+    if eod_path is None:
+        eod_path = '.'
+
+    if args.prepare:
+        prepare_data(eod_path)
+
+    if args.generate:
+        generate_data(eod_path)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
@@ -100,5 +112,7 @@ if __name__ == '__main__':
                                      )
 
     parser.add_argument('--eod-path', type=str, help='path to eod data')
+    parser.add_argument('--prepare', dest='prepare', action='store_true', help='prepare phase')
+    parser.add_argument('--generate', dest='generate', action='store_true', help='generate phase')
     args = parser.parse_args()
     main(args)
