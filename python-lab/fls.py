6b0d9cf2b9e04dca7033ec0eab224cc516b530ae
Christophe <ch.alexandre@bluewin.ch>
2017-01-17 19:47:45 +0100

    dev

--- python-lab/fls.py
+++ python-lab/fls.py
@@ -1,8 +1,5 @@
 import argparse
-import json
 import logging
-import os
-from datetime import timedelta
 import pandas
 import random
 import math
@@ -47,17 +44,23 @@ class FlexibleLeastSquare(object):
         self.v_epsilon = v_epsilon
         self.v_w = numpy.identity(size + 1) * delta / (1. - delta)
 
-    def adjust(self, output, inputs):
-        r = self.p + self.v_w
+    def estimate(self, output, inputs):
+        """
+
+        :param output: scalar value of measured output
+        :param inputs: list of measured input values
+        :return:
+        """
+        var_state_error = self.p + self.v_w
         factors = numpy.matrix(inputs + [1.]).transpose()
         output = numpy.matrix([output])
         output_estimated = factors.transpose() * self.beta
-        q = factors.transpose()* r * factors + self.v_epsilon
+        var_output_error = factors.transpose()* var_state_error * factors + self.v_epsilon
         error = output - output_estimated
-        kalman_gain = r * factors / q
+        kalman_gain = var_state_error * factors / var_output_error
         self.beta += kalman_gain * error
-        self.p = r - kalman_gain * factors.transpose() * r
-        return output_estimated.item(), self.beta, math.sqrt(q)
+        self.p = var_state_error - kalman_gain * factors.transpose() * var_state_error
+        return output_estimated.item(), self.beta, math.sqrt(var_output_error)
 
 
 def main(args):
@@ -77,7 +80,7 @@ def main(args):
         y = combine(v1, v2, v3, epsilon)
 
         # FLS
-        output_estimated, beta, var = fls.adjust(y, [v1, v2, v3])
+        output_estimated, beta, var = fls.estimate(y, [v1, v2, v3])
         logging.info('beta:\n%s' % beta)
         logging.info('var: %s' % var)
         state = {'y': y, 'signal': output_estimated, 'v1': v1, 'v2': v2, 'v3': v3, 'noise': epsilon}
