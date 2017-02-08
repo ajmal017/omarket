7b9e16e430c8f392e48c4f9a0b4345f07a0665bc
Christophe <ch.alexandre@bluewin.ch>
2017-02-08 14:22:42 +0100

    dev

--- python-lab/src/example-kalman.py
+++ python-lab/src/example-kalman.py
@@ -1,8 +1,10 @@
 import argparse
+import csv
 import logging
 import math
 import numpy
 import os
+from datetime import date
 from matplotlib import pyplot
 import pandas
 from statsmodels.formula.api import OLS
@@ -21,9 +23,9 @@ class PositionAdjuster(object):
         self.current_quantities = [0.] * len(securities)
         self.scaling = scaling
 
-    def move_to(self, timestamp, weights, close_prices, target_position):
+    def move_to(self, timestamp, weights, prices, target_position):
         logging.info('moving to position: %d at %s' % (target_position, timestamp.strftime('%Y-%m-%d')))
-        for count, weight_price in enumerate(zip(weights, close_prices)):
+        for count, weight_price in enumerate(zip(weights, prices)):
             weight, price = weight_price
             target_weight = target_position * weight
             target_quantity = round(self.scaling * target_weight / price)
@@ -43,28 +45,29 @@ class PositionAdjuster(object):
 
 class Trader(object):
 
-    def __init__(self, scaling, securities):
+    def __init__(self, scaling, securities, step_size):
         self.current_level = 0.
         self.signal_zone = 0
         self.position_adjuster = PositionAdjuster(securities, scaling)
         self.equity = list()
         self.deviation = 0.
+        self._step_size = step_size
 
-    def update_state(self, timestamp, signal, deviation, weights, close_prices):
+    def update_state(self, timestamp, signal, deviation, weights, traded_prices):
         if deviation == 0.:
             return
 
-        self.deviation = deviation
+        self.deviation = deviation * self._step_size
 
         if signal >= self.level_sup():
             self.signal_zone += 1
-            self.position_adjuster.move_to(timestamp, weights, close_prices, self.signal_zone)
+            self.position_adjuster.move_to(timestamp, weights, traded_prices, self.signal_zone)
 
         if signal <= self.level_inf():
             self.signal_zone -= 1
-            self.position_adjuster.move_to(timestamp, weights, close_prices, self.signal_zone)
+            self.position_adjuster.move_to(timestamp, weights, traded_prices, self.signal_zone)
 
-        self.equity.append({'date': timestamp, 'pnl': self.position_adjuster.get_nav(close_prices)})
+        self.equity.append({'date': timestamp, 'pnl': self.position_adjuster.get_nav(traded_prices)})
 
     def level_inf(self):
         return (self.signal_zone - 1) * self.deviation
@@ -161,6 +164,10 @@ class RegressionModelOLS(object):
         self.current_y = y_value
 
     def get_residual_error(self):
+        """
+        Standard error of estimates.
+        :return:
+        """
         return math.sqrt(self.result.mse_resid)
 
     def get_factors(self):
@@ -184,35 +191,41 @@ class RegressionModelOLS(object):
         return self.current_y - self.get_estimate()
 
 
-def process_with_regression(securities, prices, regression, warmup_period):
+def process_with_regression(securities, signal_data, regression, warmup_period, prices_by_security):
     chart_bollinger = list()
     chart_beta = list()
     chart_regression = list()
-    signal_fls = 0.
-    trader_fls = Trader(scaling=10000., securities=securities)
-
-    for count, row in enumerate(prices.iterrows()):
+    signal = 0.
+    trader_engine = Trader(scaling=10000., securities=securities, step_size=2)
+    for count, rows in enumerate(zip(signal_data.iterrows(), signal_data.shift(-1).iterrows())):
+        row, row_next = rows
         row_count, price_data = row
+        row_count_next, price_data_next = row_next
         timestamp = price_data['date']
         dependent_price, independent_prices = price_data[securities[0]], price_data[securities[1:]]
         regression.compute_regression(dependent_price, independent_prices.tolist())
         dev_fls = regression.get_residual_error()
-        deviation = 2. * dev_fls
-        #deviation = 3.  # DEBUG
+        level_inf = trader_engine.level_inf()
+        level_sup = trader_engine.level_sup()
+        next_date = price_data_next['date']
+        if count > warmup_period and not numpy.isnan(price_data_next[securities[0]]):
+            weights = regression.get_weights()
+            signal = regression.get_residual()
+            traded_prices = list()
+            for security in securities:
+                prices = prices_by_security[security][prices_by_security[security].index == next_date]
+                if len(prices) == 0:
+                    logging.error('no data as of %s' % (next_date))
 
-        close_prices = [dependent_price] + independent_prices.tolist()
-        if count > warmup_period:
-            weights_fls = regression.get_weights()
-            signal_fls = regression.get_residual()
-            trader_fls.update_state(timestamp, signal_fls, deviation, weights_fls, close_prices)
+                traded_prices.append(prices['open'].values[0])
+
+            trader_engine.update_state(timestamp, signal, dev_fls, weights, traded_prices)
 
         signal_data = {
             'date': timestamp,
-            'limit_inf': -deviation,
-            'limit_sup': deviation,
-            'level_inf': trader_fls.level_inf(),
-            'level_sup': trader_fls.level_sup(),
-            'signal_fls': signal_fls}
+            'level_inf': level_inf,
+            'level_sup': level_sup,
+            'signal_fls': signal}
         chart_bollinger.append(signal_data)
 
         beta_data = dict()
@@ -225,7 +238,7 @@ def process_with_regression(securities, prices, regression, warmup_period):
         regression_data = {'date': timestamp, 'y*': regression.get_estimate(), 'y': dependent_price, 'portfolio': regression.get_estimate() - regression.get_factors()[-1]}
         chart_regression.append(regression_data)
 
-    trader_fls.get_equity().plot()
+    trader_engine.get_equity().plot()
     pandas.DataFrame(chart_beta).set_index('date').plot(subplots=True)
     pandas.DataFrame(chart_bollinger).set_index('date').plot(subplots=False)
     #pandas.DataFrame(chart_regression).set_index('date').plot(subplots=False)
@@ -233,29 +246,34 @@ def process_with_regression(securities, prices, regression, warmup_period):
 
 def main(args):
     pyplot.style.use('ggplot')
-    prices_path = os.sep.join(['..', 'resources','eod.pkl'])
-    all_prices = pandas.read_pickle(prices_path)
 
     #securities = ['PCX/' + symbol for symbol in ['PFF','XLV','XRT']]
     #securities = ['PCX/' + symbol for symbol in ['SPY','VOO']]
     #securities = ['PCX/' + symbol for symbol in ['IAU','GDX']]
     #securities = ['PCX/' + symbol for symbol in ['SPY','IWM']]
-    securities = ['PCX/' + symbol for symbol in ['SPY','UUP','VOO']]
+    #securities = ['PCX/' + symbol for symbol in ['SPY','UUP','VOO']]
     #securities = ['PCX/' + symbol for symbol in ['VWO','XLB','XLI']]
     #securities = ['PCX/' + symbol for symbol in ['EFA','SCHF','VEU']]
     #securities = ['PCX/' + symbol for symbol in ['EEM','XLB','XLI']]
     #securities = ['PCX/' + symbol for symbol in ['VEA','VEU','VWO']]
     #securities = ['PCX/' + symbol for symbol in ['IWD','XLE','XOP']]
-    #securities = ['PCX/' + symbol for symbol in ['USMV','XLU','XLY']]
+    securities = ['PCX/' + symbol for symbol in ['USMV','XLU','XLY']]
     #securities = ['PCX/' + symbol for symbol in ['GDX','SLV','XLU']]
     #securities = ['PCX/' + symbol for symbol in ['GDX','IAU','KRE']]
     #securities = ['PCX/' + symbol for symbol in ['AGG','BND','PGX']]
     #securities = ['PCX/' + symbol for symbol in ['AGG','IAU','PFF']]
 
+    prices_path = os.sep.join(['..', '..', 'data', 'eod'])
+    prices_by_security = dict()
+    prices = pandas.DataFrame()
+    for security in securities:
+        exchange, security_code = security.split('/')
+        prices_df = load_prices(prices_path, exchange, security_code)
+        prices_by_security[security] = prices_df
+        prices[security] = prices_df['close adj']
 
-    prices = all_prices[securities]
     prices.reset_index(inplace=True)
-    in_sample_prices = prices  # [prices['date'] < date(2015, 1, 1)]
+    signal_data = prices[prices['date'] < date(2015, 1, 1)]
 
     warmup_period = 10
 
@@ -267,19 +285,52 @@ def main(args):
     #process_with_regression(securities, in_sample_prices, regression0, warmup_period)
     #process_with_regression(securities, in_sample_prices, regression1, warmup_period)
     #process_with_regression(securities, in_sample_prices, regression2, warmup_period)
-    process_with_regression(securities, in_sample_prices, regression10, warmup_period)
+    process_with_regression(securities, signal_data, regression10, warmup_period, prices_by_security)
     pyplot.show()
+
+
+def load_prices(prices_path, exchange, security_code):
+    letter = security_code[0]
+    dir_path = os.sep.join([prices_path, exchange, letter, security_code])
+    logging.info('accessing prices from: %s' % str(os.path.abspath(dir_path)))
+    prices_df = None
+    for root, dir, files in os.walk(dir_path):
+        csv_files = [csv_file for csv_file in files if csv_file.endswith('.csv')]
+        for csv_file in sorted(csv_files):
+            with open(os.sep.join([root, csv_file]), 'r') as csv_data:
+                fields = ['date', 'open', 'high', 'low', 'close', 'close adj', 'volume']
+                reader = csv.DictReader(csv_data, fieldnames=fields)
+                data = list()
+                type_fields = {
+                    'date': lambda yyyymmdd: date(int(yyyymmdd[:4]), int(yyyymmdd[4:6]), int(yyyymmdd[6:])),
+                    'open': float, 'high': float, 'low': float, 'close': float, 'close adj': float,
+                    'volume': int
+                }
+                for row in reader:
+                    converted_row = {key: type_fields[key](row[key]) for key in row}
+                    data.append(converted_row)
+
+                if prices_df is None:
+                    prices_df = pandas.DataFrame(data)
+                    prices_df.set_index('date')
+
+                else:
+                    prices_df = prices_df.append(data, ignore_index=True)
+
+    dividends = prices_df['close'].shift(1) * prices_df['close adj'] / prices_df['close adj'].shift(1) - prices_df['close']
+    prices_df['dividend'] = dividends[dividends > 1E-4]
+    return prices_df.set_index('date')
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
-    file_handler = logging.FileHandler('example-kalman.log', mode='w')
+    file_handler = logging.FileHandler('backtest.log', mode='w')
     formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
     file_handler.setFormatter(formatter)
     logging.getLogger().addHandler(file_handler)
     logging.info('starting script')
-    parser = argparse.ArgumentParser(description='Running Kalman Algo.',
+    parser = argparse.ArgumentParser(description='Backtesting prototype.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter
                                      )
-
     args = parser.parse_args()
     main(args)
+
