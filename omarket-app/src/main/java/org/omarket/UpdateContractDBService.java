7eb74397b56eb17681c4acec53b5de2aa98ba88d
Christophe <ch.alexandre@bluewin.ch>
2018-04-15 18:50:50 +0200

    dev

--- omarket-app/src/main/java/org/omarket/UpdateContractDBService.java
+++ omarket-app/src/main/java/org/omarket/UpdateContractDBService.java
@@ -4,16 +4,19 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.json.JsonObject;
+import io.vertx.rx.java.ObservableFuture;
 import io.vertx.rxjava.core.RxHelper;
 import io.vertx.rxjava.core.Vertx;
+import io.vertx.rxjava.core.eventbus.Message;
 import lombok.extern.slf4j.Slf4j;
-import org.omarket.trading.ContractFetcher;
 import org.omarket.trading.verticles.MarketDataVerticle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import rx.Observable;
+import rx.functions.Func1;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
@@ -27,7 +30,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
-@Component
+@Service
 public class UpdateContractDBService {
 
     @Value("${address.contract_download}")
@@ -40,6 +43,9 @@ public class UpdateContractDBService {
         this.marketDataVerticle = marketDataVerticle;
     }
 
+    /**
+     * Service entry point.
+     */
     public void update() {
         VertxOptions options = new VertxOptions();
         options.setBlockedThreadCheckInterval(1000);
@@ -47,7 +53,7 @@ public class UpdateContractDBService {
         final Vertx vertx = Vertx.vertx(options);
         Observable<String> marketDataDeployment = RxHelper.deployVerticle(vertx, marketDataVerticle);
         marketDataDeployment.flatMap(deploymentId -> {
-            log.info("succesfully deployed market data verticle: " + deploymentId);
+            log.info("succesfully deployed ContractInfo verticle: " + deploymentId);
             Observable<String> codesStream = Observable.empty();
             try {
                 URL etfsResource = Thread.currentThread().getContextClassLoader().getResource("etfs.json");
@@ -84,5 +90,32 @@ public class UpdateContractDBService {
                     log.info("completed");
                     System.exit(0);
                 });
+    }
+
+    /**
+     * Created by Christophe on 04/01/2017.
+     */
+    private static class ContractFetcher implements Func1<String, Observable<? extends Message<JsonObject>>> {
+
+
+        private final String addressContractDownload;
+        private final Vertx vertx;
+
+        public ContractFetcher(Vertx vertx, String addressContractDownload) {
+            this.addressContractDownload = addressContractDownload;
+            this.vertx = vertx;
+        }
+
+        @Override
+        public Observable<? extends Message<JsonObject>> call(String code) {
+            JsonObject contractCode = new JsonObject().put("conId", code);
+            ObservableFuture<Message<JsonObject>> contractStream =
+                    io.vertx.rx.java.RxHelper.observableFuture();
+            DeliveryOptions deliveryOptions = new DeliveryOptions();
+            deliveryOptions.setSendTimeout(10000);
+            vertx.eventBus().send(addressContractDownload, contractCode,
+                    deliveryOptions, contractStream.toHandler());
+            return contractStream;
+        }
     }
 }
