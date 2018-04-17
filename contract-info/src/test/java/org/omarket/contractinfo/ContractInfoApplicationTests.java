3d410b25472013c41d1349f5c29e2d79e02ed04e
Christophe <ch.alexandre@bluewin.ch>
2018-04-17 08:11:58 +0200

    dev

---
+++ contract-info/src/test/java/org/omarket/contractinfo/ContractInfoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.omarket.contractinfo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContractInfoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ price-feed/src/main/java/org/omarket/pricefeed/PriceFeedApplication.java
@@ -0,0 +1,12 @@
+package org.omarket.pricefeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceFeedApplication.class, args);
+	}
+}
